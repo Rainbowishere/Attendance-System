c8f388910e738b83c408d7e27cf1d6e5b2670e13
Rainbow <Rainbow@RAINBOW-LAPTOP>
2018-08-22 08:50:17 +0700

    Add project files.

---
+++ Attendance System/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Attendance_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Attendance_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        private AttendanceDatabaseContext db = new AttendanceDatabaseContext();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult LoadEmployee(string id)
+        {
+            //var person = db.People.Find(id);
+            return PartialView(db.People.Find(id));
+        }
+    }
+}
---
+++ Attendance System/Controllers/People1Controller.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Attendance_System.Models;
+
+namespace Attendance_System.Controllers
+{
+    public class People1Controller : Controller
+    {
+        private AttendanceDatabaseContext db = new AttendanceDatabaseContext();
+
+        // GET: People1
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.People.ToListAsync());
+        }
+
+        // GET: People1/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = await db.People.FindAsync(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // GET: People1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "PhoneNumberID,FullName,Source,EmployeeID,Device")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.People.Add(person);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        // GET: People1/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = await db.People.FindAsync(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "PhoneNumberID,FullName,Source,EmployeeID,Device")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        // GET: People1/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = await db.People.FindAsync(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Person person = await db.People.FindAsync(id);
+            db.People.Remove(person);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
