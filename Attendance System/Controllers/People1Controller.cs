f6756fe24d400e206ad1843ea45d7812a96009b8
Phoudthaxay Sisomboun <phoudthaxaysisomboun@outlook.com>
2018-08-23 09:23:14 +0700

    Changes

--- Attendance System/Controllers/People1Controller.cs
+++ Attendance System/Controllers/People1Controller.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using Attendance_System.Models;
 using System.Data.Entity;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Net;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
-using Attendance_System.Models;
 
 namespace Attendance_System.Controllers
 {
     public class People1Controller : Controller
     {
-        private AttendanceDatabaseContext db = new AttendanceDatabaseContext();
+        private AttendanceDbContext db = new AttendanceDbContext();
 
         // GET: People1
         public async Task<ActionResult> Index()
@@ -44,7 +39,7 @@ namespace Attendance_System.Controllers
 
         // POST: People1/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "PhoneNumberID,FullName,Source,EmployeeID,Device")] Person person)
@@ -76,7 +71,7 @@ namespace Attendance_System.Controllers
 
         // POST: People1/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "PhoneNumberID,FullName,Source,EmployeeID,Device")] Person person)
