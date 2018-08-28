f2490acc81ed7b6fb0993e5aea192c54ef3f50d1
Phoudthaxay Sisomboun <phoudthaxaysisomboun@outlook.com>
2018-08-28 09:33:26 +0700

    Set CheckOut

--- Attendance System/Controllers/LoginController.cs
+++ Attendance System/Controllers/LoginController.cs
@@ -1,11 +1,9 @@
 ﻿using Attendance_System.Models;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Attendance_System.Controllers
@@ -20,11 +18,15 @@ namespace Attendance_System.Controllers
             return View();
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult LoadEmployee(string id)
         {
             var existingUser = db.People.Find(id);
-            var CheckinUser = db.CheckinCheckouts.Find(id);
+            var isCheckedIn = db.CheckinCheckouts.Where(x => x.PhoneNumberID == id && x.Checkin != null && x.Checkout == null).Select(x => x.ID).FirstOrDefault();
 
             if (existingUser == null)
             {
@@ -32,69 +34,58 @@ namespace Attendance_System.Controllers
             }
             else
             {
-                return PartialView(existingUser);
-            }
+                if (isCheckedIn == default(Guid))
+                {
+                    return PartialView(existingUser);
+                }
+                else
+                {
+                    return PartialView("Checkout", isCheckedIn);
+                }
 
-            //var person = db.People.Find(id);
-            
+            }
         }
 
-        // POST: CheckinCheckouts/Create
+        // POST: CheckinCheckouts1/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> LoadEmployee(string id, [Bind(Include = "ID,PhoneNumberID,Checkin,Checkout,Purpose,Device")] CheckinCheckout checkinCheckout)
-        {
-            //if (ModelState.IsValid)
-            //{
-            //    checkinCheckout.ID = Guid.NewGuid();
-            //    checkinCheckout.Checkin = DateTimeOffset.Now;
-            //    db.CheckinCheckouts.Add(checkinCheckout);
-            //    await db.SaveChangesAsync();
-            //    return RedirectToAction("Index");
-            //}
-            checkinCheckout.ID = Guid.NewGuid();
-            checkinCheckout.PhoneNumberID = db.People.Find(id).PhoneNumberID;
-            checkinCheckout.Checkin = DateTimeOffset.Now;
-            db.CheckinCheckouts.Add(checkinCheckout);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-
-            //return View(checkinCheckout);
-        }
-
-        // GET: CheckinCheckouts1/Details/5
-        public async Task<ActionResult> Checkout(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CheckinCheckout checkinCheckout = await db.CheckinCheckouts.FindAsync(id);
-            if (checkinCheckout == null)
-            {
-                return HttpNotFound();
-            }
-            return View(checkinCheckout);
-        }
-
-        // POST: CheckinCheckouts1/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Checkout([Bind(Include = "ID,PhoneNumberID,Checkin,Checkout,Purpose,Device")] CheckinCheckout checkinCheckout)
+        public async Task<ActionResult> CheckIn([Bind(Include = "ID,PhoneNumberID,Checkin,Checkout,Purpose,Device,Purpose2")] CheckinCheckout checkinCheckout,string Purpose2)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(checkinCheckout).State = EntityState.Modified;
+                if (checkinCheckout.Purpose == "ອື່ນໆ...")
+                {
+                    checkinCheckout.Purpose = Purpose2;
+                }
+
+                checkinCheckout.ID = Guid.NewGuid();
+                checkinCheckout.Checkin = DateTimeOffset.Now;
+                db.CheckinCheckouts.Add(checkinCheckout);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            ViewBag.PhoneNumberID = new SelectList(db.People, "PhoneNumberID", "FullName", checkinCheckout.PhoneNumberID);
-            return View(checkinCheckout);
+            
+            return View("index");
         }
 
+
+        // POST: People1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register([Bind(Include = "PhoneNumberID,FullName,Source,EmployeeID,Device")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.People.Add(person);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
     }
 }
