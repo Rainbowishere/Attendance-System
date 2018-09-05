e7ddceb0a9cddf7f478bc36d20cf82c2165f0165
Rainbowishere <loungnapha91@gmail.com>
2018-09-05 10:19:37 +0700

    PeopleController

--- Attendance System/Controllers/PeopleController.cs
+++ Attendance System/Controllers/PeopleController.cs
@@ -1,22 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Attendance_System.Models;
 
 namespace Attendance_System.Controllers
 {
     public class PeopleController : Controller
     {
+        private AttendanceDbContext db = new AttendanceDbContext();
+
         // GET: People
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.People.ToListAsync());
+        }
+
+        // GET: People/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = await db.People.FindAsync(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
         {
             return View();
         }
 
-        public ActionResult Register()
+        // POST: People/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "PhoneNumberID,FullName,Source,EmployeeID,IsActive")] Person person)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.People.Add(person);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        // GET: People/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = await db.People.FindAsync(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "PhoneNumberID,FullName,Source,EmployeeID,IsActive")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        // GET: People/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = await db.People.FindAsync(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Person person = await db.People.FindAsync(id);
+            db.People.Remove(person);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
